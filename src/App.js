ad20ac95c024536a52ce6b97f19b50fa0df6c251
ma-hi-ka <mahika1801@gmail.com>
2025-05-29 16:27:04 +0800

    .

--- src/App.js
+++ src/App.js
@@ -1,25 +1,10 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import HomePage from './HomePage';
+import './App.css';        // optional, just for CRA’s default styles
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  const name = 'Alex';
+  return <HomePage userName={name} />;
 }
 
 export default App;
