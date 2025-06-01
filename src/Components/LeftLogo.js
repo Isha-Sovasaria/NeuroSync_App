eb31be9d3e2c07f366d8ebf0e06775ce3a30e605
ma-hi-ka <mahika1801@gmail.com>
2025-06-01 23:35:31 +0800

    fixed css for login/signin

--- src/Components/LeftLogo.js
+++ src/Components/LeftLogo.js
@@ -2,22 +2,40 @@ import React from 'react'
 
 export default function LeftLogo({isLogin}) {
   return (
+    // <div className="left-panel">
+    //   <div className="logo-wrapper">
+    //     <img src="/NeuroSync.png" alt="NeuroSync Logo" className="logo-img"/>
+    //     <div className="tagline">Your mental health companion</div>
+    //   </div>
+      
+    //   <div className="dolphin-wrapper"></div>
+    //     <img src="/clear_dolphin.png" alt="Dolphin" className="dolphin-img" />
+    //     <p className="welcome-text">{isLogin?"Welcome Back!":"Welcome"}</p>
+    // </div>
+
+
+    // <div className="left-panel">
+    //   <div className="logo-wrapper">
+    //     <img src="/NeuroSync.png" alt="NeuroSync Logo" className="logo-img" />
+    //     <div className="tagline">Your mental health companion</div>
+    //   </div>
+
+    //   <div className="dolphin-wrapper">
+    //     <img src="/clear_dolphin.png" alt="Dolphin" className="dolphin-img" />
+    //     <div className="speech-bubble">{isLogin ? "Welcome Back!" : "Welcome!"}</div>
+    //   </div>
+    // </div>
+
     <div className="left-panel">
       <div className="logo-wrapper">
-        <img
-        src="/NeuroSync.png"
-        alt="NeuroSync Logo"
-        className="logo-img"
-        />
-        <h3 className="tagline">Your mental health companion</h3>
+        <img src="/NeuroSync.png" alt="NeuroSync Logo" className="logo-img" />
+        <div className="tagline">Your mental health companion</div>
       </div>
-        <img
-          src="/clear_dolphin.png"
-          alt="Dolphin"
-          className="dolphin-img"
-        />
-        <p className="welcome-text">{isLogin?"Welcome Back!":"Welcome"}</p>
-    
+      
+      <img src="/clear_dolphin.png" alt="Dolphin" className="dolphin-img" />
+      <div className="welcome-text">{isLogin ? "Welcome Back!" : "Welcome!"}</div>
     </div>
   )
 }
+
+
