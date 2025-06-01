069ef3f0eece4c495d2aff565f0f765a7a1f3bc6
ma-hi-ka <mahika1801@gmail.com>
2025-06-01 22:39:41 +0800

    latest

--- src/Components/LeftLogo.js
+++ src/Components/LeftLogo.js
@@ -3,24 +3,21 @@ import React from 'react'
 export default function LeftLogo({isLogin}) {
   return (
     <div className="left-panel">
-       <div className="logo-wrapper">
-  <img
-    src="/NeuroSync.png"
-    alt="NeuroSync Logo"
-    className="logo-img"
-  />
-  <p className="tagline">Your mental health companion</p>
-
+      <div className="logo-wrapper">
         <img
-          src="/dolphin.png"
+        src="/NeuroSync.png"
+        alt="NeuroSync Logo"
+        className="logo-img"
+        />
+        <h3 className="tagline">Your mental health companion</h3>
+      </div>
+        <img
+          src="/clear_dolphin.png"
           alt="Dolphin"
           className="dolphin-img"
         />
-
-        <h2 className="welcome-text">{isLogin?"Welcome Back!":"Welcome"}</h2>
+        <p className="welcome-text">{isLogin?"Welcome Back!":"Welcome"}</p>
     
     </div>
-    </div>
-    
   )
 }
